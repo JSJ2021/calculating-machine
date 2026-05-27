3e48dfa51477c41482413a22ba150a97ccf5e9e6
Soung Jea Joung <sjoung@c1r11s6.42gyeongsan.kr>
2026-05-27 16:20:03 +0900

    creaiting prototype1

--- main.c
+++ main.c
@@ -13,36 +13,96 @@ typedef enum {
 typedef struct s_token
 {
     cal_type type;
-    char num;
+    int num;
     struct s_token *left;
     struct s_token *right;    
 } t_token;
 
-t_token *parser(char *s, int i)
+t_token *create_num(int n)
 {
-    
-    if (isdigit(s[i]))
+    t_token *node = malloc(sizeof(t_token));
+    node->type = num;
+    node->num = n;
+    node->left = 0;
+    node->right = 0;
+    return node;
+}
+
+t_token *init_token(cal_type type, t_token *left, t_token *right)
+{
+    t_token *result = (t_token *)malloc(sizeof(t_token));
+    result->left = left;
+    result->right = right;
+    result->type = type;
+    return (result);
+}
+
+t_token *parser_num(char *s, int *i)
+{
+    t_token *token = create_num(s[*i] - 48);
+    (*i)++;
+    return (token);
+}
+
+t_token *parser_trem(char *s, int *i)
+{
+    t_token *left = parser_num(s, i);
+    t_token *right;
+    while (s[*i] == '*')
     {
-        t_token *token = (t_token *)malloc(sizeof(t_token));
-        token->type = num;
-        token->num = s[i] - 48;
+        (*i)++;
+        right = parser_num(s, i);
+        left = init_token(multiplication, left, right);
+    }
+    return left;
+}
+
+t_token *parser(char *s, int *i)
+{
+    t_token *left = parser_trem(s, i);
+    t_token *right;
+    while (s[*i] == '+')
+    {
+        (*i)++;
+        right = parser_trem(s, i);
+        left = init_token(add, left, right);
+    }
+    return left;
+}
+
+int operating(t_token *token)
+{
+    int num1 = 0;
+    int num2 = 0;
+    int result = 0;
+    if (token->type == num)
+        return (token->num);
+    else if (token->left != 0 && token->right != 0)
+    {
+        num1 = operating(token->left);
+        num2 = operating(token->right);
     }
     else
+        return (-1);
+    
+    if (token->type == add)
+        result = (num1 + num2);
+    else if (token->type == multiplication)
     {
-        t_token *token = (t_token *)malloc(sizeof(t_token));
-        if(s[i] == '+')
-        {
-            token->type = add;
-        }
-        else if(s[i] == '*')
-        {
-            token->type = multiplication;
-        }
-        
+        result = (num1 * num2);
     }
+    if (num1 == -1 || num2 == -1)
+        return (-1);
+    return (result);
 }
-int main(int argc, char **argv)
-{
-    t_token *token;
 
+int main()
+{
+    int result;
+    int i = 0;
+    t_token *token = parser("1+1*2*5+4", &i);
+    result = operating(token);
+    if(result != -1)
+        printf("%d\n", result);
+    return (0);
 }
